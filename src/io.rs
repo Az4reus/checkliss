6809e4eaeb073939c4ef27267c01bb672544d59d
Az4reus <az4reus@gmail.com>
2017-01-04 14:42:39 +0100

    Adds some isolated IO for convenience

---
+++ src/io.rs
@@ -0,0 +1,44 @@
+use std::io;
+use std::fs::OpenOptions;
+use std::io::Write;
+use std::env::current_dir;
+
+pub fn save_string_locally(content: String, filename: &str) -> Result<(), io::Error> {
+    let mut local_filename = current_dir()
+        .expect("Stop launching this without a PWD");
+
+    local_filename.push(filename);
+
+    let file = OpenOptions::new()
+        .create(true)
+        .write(true)
+        .open(local_filename)
+        .unwrap();
+
+    match file {
+        Ok(mut file) => {
+            match file.write(content.as_bytes()) {
+                Ok(_) => Ok(()),
+                Err(e) => Err(e)
+            }
+        },
+        Err(e) => Err(e)
+    }
+}
+
+#[cfg(test)]
+mod test{
+
+    #[test]
+    fn test_local_saving() {
+        use std::fs::metadata;
+        use std::env::current_dir;
+
+        save_string_locally("foobar".to_owned(), "temp").unwrap();
+
+        let mut local_path = current_dir();
+        local_path.push("temp");
+
+        assert!(metadata(local_path).unwrap().is_file())
+    }
+}
