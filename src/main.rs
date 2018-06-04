273a20c3371152b9d1470b6f2409d3d352bf1440
Mordecai Malignatus <az4reus@gmail.com>
2018-06-04 12:05:55 +0200

    Feat(tests): Removes garbage test.

--- src/main.rs
+++ src/main.rs
@@ -43,34 +43,4 @@ mod test {
         // Yes, this is a test dependent on circumstances uncontrollable by the program itself. Sue me.
         assert!(has_xetex())
     }
-
-    #[test]
-    fn test_integration() {
-        // if it doesn't die somewhere, we're good.
-
-        use std::fs::remove_file;
-        use std::env::current_dir;
-
-        use item::Item;
-        use tex::compile_tex;
-
-        let test_node = Item::new("test parent".to_owned()).add_child(Item::new("test child".to_owned()));
-
-        compile_tex(test_node);
-
-        let mut aux = current_dir().unwrap();
-        let mut log = current_dir().unwrap();
-        let mut pdf = current_dir().unwrap();
-        let mut tex = current_dir().unwrap();
-
-        aux.push("temp.aux");
-        log.push("temp.log");
-        pdf.push("temp.pdf");
-        tex.push("temp.tex");
-
-        remove_file(aux).unwrap();
-        remove_file(log).unwrap();
-        remove_file(pdf).unwrap();
-        remove_file(tex).unwrap();
-    }
 }
